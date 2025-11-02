1df1df78a1435599d2fbe420ea273fb2e35a3c91
shambu2 <mugalishambu601@gmail.com>
2025-11-02 11:47:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ app.get('/', (req, res) => {
 
 // Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+app.listen(PORT,"0.0.0.0" () => {
   console.log(`Server running on port ${PORT}`);
 });
